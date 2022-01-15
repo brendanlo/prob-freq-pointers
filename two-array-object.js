06cbe112e7a98f6b9f279574c2ce9e221a9b0f01
prokid20000 <95936374+prokid20000@users.noreply.github.com>
2022-01-14 20:15:36 -0500

    Workedon two-array object

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,22 @@
+"use strict"
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+
+/*Write a function called twoArrayObject which accepts two arrays of varying lengths. The first array consists of keys and the second one consists of values. 
+Your function should return an object created from the keys and values. If there are not enough values, the rest of keys should have a value of null. 
+If there not enough keys, just ignore the rest of values. */
+
+/** Function takes array of keys and array of values and then return object of key and value pairs. */
+function twoArrayObject(keys, values) {
+    let obj = {};
+    // Iterate through array of key so that it can ignore excess amount of values if there are.
+    for (let i = 0; i < keys.length; i++) {
+        let key = keys[i];
+        let value = values[i];
+        // If there are excess amount of keys, assign them with value of null.
+        if (value == undefined) {
+            value = null;
+        }
+        obj[key] = value;
+    }
+    return obj;
 }
