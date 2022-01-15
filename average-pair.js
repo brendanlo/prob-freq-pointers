ac5637e5039147f747d7efc3b56cae73f7f0ac23
prokid20000 <95936374+prokid20000@users.noreply.github.com>
2022-01-14 19:29:17 -0500

    Working on average-pair

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,30 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+
+/* Write a function called averagePair. 
+Given a sorted array of integers and a target average, determine if there is a pair of values in the array where the average of the pair equals the target average. 
+There may be more than one pair that matches the average target. */
+
+// Input will be array of numbers and target number.
+function averagePair(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    // Make sure left end doesn't exceed right end
+    while (left < right) {
+        if ((nums[left] + nums[right])/2 == target) {
+            return true;
+        }
+        //If average of pair is larger than target, decrease right index by 1.
+        else if ((nums[left] + nums[right])/2 > target) {
+            right--;
+        }
+        //If average of pair is less than target, increase left index by 1.
+        else {
+            left++;
+        }
+    }
+    //If there's no pair return false.
+    return false;
+
+
 }
